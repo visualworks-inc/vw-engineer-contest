83b80ff8b3db20fa6e506188b72ebac0289ce340
yasunori1005 <suzukiyasunori1005@gmail.com>
2023-09-02 10:46:27 +0900

    vw-contest

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -14,6 +14,16 @@ class Application extends Model
      *
      * @param string $name
      */
+
+    public static function convertToFullWidthDash($input)
+    {
+        $differentHyphenArray = ['-', 'ー', '‐', '‑', '–', '—', '―', '−', 'ｰ'];
+        $hyphen = 'ー'; // 全角の長音に変換
+        $replaceInput = str_replace($differentHyphenArray,$hyphen,$input);
+        return $replaceInput;
+    }
+
+
     public static function is_used_name(string $name): bool
     {
         $user = self::where('name', $name)->first();
